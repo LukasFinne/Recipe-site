1bfc70064be153e1b2cbb017a3c127096e1ca8ef
Lukas <lukas.ikvm1@gmail.com>
2022-09-03 14:44:27 +0200

    Add fetch to recipe pages

--- js/recipes.js
+++ js/recipes.js
@@ -8,31 +8,86 @@ const appendNode = (parent, elem) => {
 
 const ul = document.querySelector(".recipe");
 const url = "data/recipes.json";
-fetch(url)
-  .then((resp) => resp.json())
-  .then(function (data) {
-    let recipes = data.recipe;
 
-    return recipes.map(function (recipe) {
-      let li = createNode("li"),
-        h2 = createNode("h2"),
-        img = createNode("img"),
-        p = createNode("p"),
-        a = createNode("a");
-      h2.textContent = recipe.name;
-      img.src = recipe.picture;
-      p.textContent = recipe.information;
-      a.textContent = "Go to recipe page";
-      a.href = recipe.recipePage;
+var path = window.location.pathname;
+var page = path.split("/").pop();
 
-      appendNode(li, h2);
-      appendNode(li, img);
-      appendNode(p, a);
-      appendNode(li, p);
+switch (page) {
+  case "recipe.html":
+    printAll(url, ul);
+    break;
+  case "frenchToast.html":
+    printFrenchToast(url, ul);
+    break;
+  case "pancake.html":
+    printPancake(url, ul);
+    break;
+  case "waffle.html":
+    printWaffle(url, ul);
+    break;
+  default:
+    console.log("error");
+}
 
-      appendNode(ul, li);
+function printFrenchToast(url, ul) {
+  fetch(url)
+    .then((resp) => resp.json())
+    .then(function (data) {
+      let recipes = data.recipe;
+      let frenchToast = recipes.slice(0, 1);
+
+      return frenchToast.map(function (recipe) {
+        let li = createNode("li"),
+          h2 = createNode("h2"),
+          img = createNode("img"),
+          p = createNode("p"),
+          a = createNode("a");
+        h2.textContent = recipe.name;
+        img.src = recipe.picture;
+        p.textContent = recipe.information;
+        a.textContent = "Go to recipe page";
+        a.href = recipe.recipePage;
+
+        appendNode(li, h2);
+        appendNode(li, img);
+        appendNode(p, a);
+        appendNode(li, p);
+
+        appendNode(ul, li);
+      });
+    })
+    .catch(function (error) {
+      console.log(error);
     });
-  })
-  .catch(function (error) {
-    console.log(error);
-  });
+}
+
+function printAll(url, ul) {
+  fetch(url)
+    .then((resp) => resp.json())
+    .then(function (data) {
+      let recipes = data.recipe;
+
+      return recipes.map(function (recipe) {
+        let li = createNode("li"),
+          h2 = createNode("h2"),
+          img = createNode("img"),
+          p = createNode("p"),
+          a = createNode("a");
+        h2.textContent = recipe.name;
+        img.src = recipe.picture;
+        p.textContent = recipe.information;
+        a.textContent = "Go to recipe page";
+        a.href = recipe.recipePage;
+
+        appendNode(li, h2);
+        appendNode(li, img);
+        appendNode(p, a);
+        appendNode(li, p);
+
+        appendNode(ul, li);
+      });
+    })
+    .catch(function (error) {
+      console.log(error);
+    });
+}
